0810f1054239c4ccff7d9514394ac0e6892c4f44
Siddharth2714 <92843916+Siddharth2714@users.noreply.github.com>
2021-10-20 12:22:43 +0530

    Add files via upload

---
+++ student detail 2714.cpp
@@ -0,0 +1,20 @@
+/*c program to print the details of a student*/
+#include<stdio.h>
+void main()
+{
+	//variable declaration//
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 19;
+	cgpa = 20.8;
+	initial = 'M';
+	
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+}
+
